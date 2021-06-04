9562035d1c97b47824dc35b3273535405cae3dc9
danielromeo <danielromeo99@gmail.com>
2021-06-04 15:37:01 +0200

    completed a simple mining state

--- blockchain.js
+++ blockchain.js
@@ -1,5 +1,4 @@
-const SHA256 = require('crypto-js/sha256');
-const Transactions = require('./transaction')
+const Transaction = require('./transaction')
 const Block = require("./block");
 
 class Blockchain{
@@ -7,23 +6,50 @@ class Blockchain{
 		this.chain = [this.createGenesisBlock()];
 		this.difficulty = 2;
 		this.pendingTransactions = [];
+		this.miningReward = 100;
 	}
 
 	createGenesisBlock(){
-		return new Block("2021/01/01", [], "0")
+		return new Block("2021/01/01", [], "0");
 	}
 
 	getLatestBlock(){
 		return this.chain[this.chain.length -1];
 	}
 
-	addBlock(newBlock){
-		newBlock.previousHash = this.getLatestBlock().hash;
-		newBlock.mineBlock(this.difficulty);
-		this.chain.push(newBlock);	
+	minePendingTransactions(miningRewardAddress){
+		let block = new Block(Date.now(), this.pendingTransactions) // currently we are just mining all the transactions that are pending
+		block.mineBlock(this.difficulty);
+
+		console.log("Block successfully mined!");
+		this.chain.push(block);
+
+		// create a new transaction(a reward for the solver of the previous block)
+		this.pendingTransactions = [
+			new Transaction(null, miningRewardAddress, this.miningReward)
+		]
 	}
 
-	// function to verify the blocks in the blochain
+	createTransaction(transaction){
+		this.pendingTransactions.push(transaction);
+	}
+
+	getBalanceOfAddress(address){
+		let balance = 0;
+
+		for( const block of this.chain){
+			for(const trans of block.transactions){
+				if(address === trans.fromAddress){
+					balance-= trans.amount;
+				}
+				if(address === trans.toAddress){
+					balance+= trans.amount;
+				}
+			}
+		}
+		return balance;
+	}
+
 	isChainValid(){
 		// traverse the entire blockchain and verify that the blocks are linked properly
 		for(let i = 1 ; i < this.chain.length; i++){
@@ -47,9 +73,13 @@ class Blockchain{
 
 
 let coin  = new Blockchain();
-coin.addBlock(new Block(Date.now(), [], {amount: 4}));
-// coin.addBlock(new Block(2, "2021/02/15", {amount: 4}));
-// coin.addBlock(new Block(3, "2021/03/10", {amount: 10000000}))
-// console.log(coin.)
+
+coin.createTransaction(new Transaction('address1', 'address2', 100 ))
+coin.createTransaction(new Transaction('address2', 'address3', 30 ))
+
+console.log('\n Starting the miner...');
+coin.minePendingTransactions('Romeo');
+
+console.log('\n Balance of Romeo is: '+ coin.getBalanceOfAddress('address2'));
 
 console.log(JSON.stringify(coin, null, 4));
