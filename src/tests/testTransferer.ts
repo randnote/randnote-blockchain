41198e6f2cf4d8fb9a4f47781b7bc81316dd3ada
danielromeo <danielromeo99@gmail.com>
2022-05-08 04:18:20 +0200

    minor changes

---
+++ src/tests/testTransferer.ts
@@ -0,0 +1,27 @@
+// THis file is to make inital deposits to the accounts that sit in the databse... no
+/*This file has the first function as a key generator... priv and public keys to help me generate keys whenever i need to. */
+
+/* ROMEO'S KEYS 
+PRIVATE:  d56f2bd3eb7c5612fbc04c96342102327c7c9ae44abf01ed5316604747d6f111
+PUBLIC:   047b1ca607797acfdd5d27173275484cf97ac0bc434a5fee5bd2a1e086cfe5ed42fc43220068120875e64713971f185fa538108b5992f494228f6865112c953453
+*/
+
+const keyGenerator = () =>{
+    let elliptic = require('elliptic');
+    // let sha3 = require('js-sha3');
+    let ec = new elliptic.ec('secp256k1');
+
+    let keyPair = ec.genKeyPair(); // Generate random keys... both the public and private
+    // console.log(keyPair)
+
+    const privKey = keyPair.getPrivate(); // get the private key
+    //console.log(privKey) // the raw private key
+    console.log(privKey.toString('hex')) // the hexedecimal value of the private key....
+
+    // now extract the public key:
+    const pubKey = keyPair.getPublic();
+    // console.log(pubKey)
+    let newpubKey = pubKey.encode('hex'); // the hex version of the public key
+    console.log(newpubKey)
+}
+
