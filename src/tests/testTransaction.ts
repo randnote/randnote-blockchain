df288af6e446664574a31d4cc016be065b57c59a
danielromeo <danielromeo99@gmail.com>
2022-05-09 01:14:02 +0200

    working on chnging pem keys

--- src/tests/testTransaction.ts
+++ src/tests/testTransaction.ts
@@ -11,10 +11,8 @@ const ec = new EC("secp256k1");
 
 const createTransaction = async () => {
 	let jsonn = {
-		fromAddress:
-			`0465f31d0233efa00f829098040de97d254922bc6730a2f59bee6525e203a5c3f10168be5391b28eb9fa81a0aa87583040c2e9542b7aad50666577b446239d6fc3`,
-		fromAddressPrivateKey:
-			`f224f9e944b73c51ee9a8140b65e8f06e1422a3fecc4c79fc8577bc80a427ce0`,
+		fromAddress: `0465f31d0233efa00f829098040de97d254922bc6730a2f59bee6525e203a5c3f10168be5391b28eb9fa81a0aa87583040c2e9542b7aad50666577b446239d6fc3`,
+		fromAddressPrivateKey: `f224f9e944b73c51ee9a8140b65e8f06e1422a3fecc4c79fc8577bc80a427ce0`,
 		toAddress: "paul",
 		amount: 10,
 	};
