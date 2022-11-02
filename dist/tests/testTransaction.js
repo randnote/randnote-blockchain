af3ca087a95fb90a55503ea49672891fbe3d5f31
danielromeo <danielromeo99@gmail.com>
2022-11-02 13:51:39 +0200

    minor changes

--- dist/tests/testTransaction.js
+++ dist/tests/testTransaction.js
@@ -53,7 +53,8 @@ var createTransaction = function () { return __awaiter(void 0, void 0, void 0, f
         jsonn = {
             fromAddress: "0465f31d0233efa00f829098040de97d254922bc6730a2f59bee6525e203a5c3f10168be5391b28eb9fa81a0aa87583040c2e9542b7aad50666577b446239d6fc3",
             fromAddressPrivateKey: "f224f9e944b73c51ee9a8140b65e8f06e1422a3fecc4c79fc8577bc80a427ce0",
-            toAddress: "paul",
+            // toAddress: "paul",
+            toAddress: "041394339bca8fcbc0db29eecef520c3694f518051f9a04985dd80a18fe8135d64721ee2025e055b102ec4e145b5df19dba0edd691bdc5c1cf1c46faa86039b684",
             amount: 10,
         };
         snack = JSON.stringify(jsonn);
